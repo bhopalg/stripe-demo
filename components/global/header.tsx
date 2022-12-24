06e938b760d76a10a24a7708dd2fef45bfa667aa
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-12-24 12:35:05 +0000

    added temp log out to header

--- components/global/header.tsx
+++ components/global/header.tsx
@@ -23,6 +23,15 @@ export default function Header() {
         <header
             className={'z-50 w-full absolute top-0 px-10 py-5 flex justify-end'}
         >
+            {user && (
+                <Link
+                    className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    href={'/api/auth/logout'}
+                >
+                    Log Out
+                </Link>
+            )}
+
             {!user && (
                 <Link
                     href={'/api/auth/login'}
